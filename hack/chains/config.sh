f8bd8c94d89107daa8ec6bbe537e43229781e80a
Rob Nester <rnester@redhat.com>
2022-04-19 14:28:48 -0400

    HACBS-228: Add support for bootstrapping rekor
    This commit moves rekor into the bootstrap script. By default the rekor
    service is not externally accessible outside the cluster.
    
    When ran with the `preview` flag, the rekor server is configured with a
    domain of the cluster in the form of `rekor.<domain>` and the
    tekton-chains configmap is patched to use this value as the
    `transparency.url` value.
    
    After this PR is merged, executing the `boostrap-cluster.sh` script
    again after having ran it with the `preview` flag will return the rekor
    instance to not being externally accessible outside of the cluster.
    
    Signed-off-by: Rob Nester <rnester@redhat.com>

--- hack/chains/config.sh
+++ hack/chains/config.sh
@@ -67,7 +67,7 @@ case "$1" in
     ;;
 
   rekor-local )
-    REKOR_SERVER=$(kubectl get ingress -n rekor-server -o yaml|yq e '.items[.spec].spec.rules[.host].host' -)
+    REKOR_SERVER=$(kubectl get ingress -n rekor -o jsonpath='{.items[0].spec.rules[0].host}'
     $0 "transparency.url: \"https://$REKOR_SERVER\"" $2
 
     ;;
