b56e59414b0ae1dfe0e409b7078394b2eb55d5d5
Xavier Coulon <xcoulon@redhat.com>
2022-11-15 10:25:16 +0100

    refactor script
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- hack/setup-monitoring.sh
+++ hack/setup-monitoring.sh
@@ -1,27 +1,80 @@
 #!/bin/bash
-# This script ensures that PROMETHEUS and GRAFANA environment variables are set and creates all required secrets to 
-# install and configure Prometheus and Grafana on the workload clusters.
 
-#Check if the variables for prometheus are set or not
-[[ -z "$PROMETHEUS_GITHUB_CLIENT_ID" ]] && { echo "Please add github oauth client id for prometheus access"; exit 1; }
-[[ -z "$PROMETHEUS_GITHUB_CLIENT_SECRET" ]] && { echo "Please add github oauth client secret for prometheus access"; exit 1; }
-[[ -z "$PROMETHEUS_GITHUB_COOKIE_SECRET" ]] && { echo "Please add github cookie secret for prometheus access"; exit 1; }
+# ----------------------------------------------------------------
+# OAuth2 Proxy Secrets
+# ----------------------------------------------------------------
 
-#Check if the variables for grafana are set or not
-[[ -z "$GRAFANA_GITHUB_CLIENT_ID" ]] && { echo "Please add github oauth client id for grafana access"; exit 1; }
-[[ -z "$GRAFANA_GITHUB_CLIENT_SECRET" ]] && { echo "Please add github oauth client secret for grafana access"; exit 1; }
-[[ -z "$GRAFANA_GITHUB_COOKIE_SECRET" ]] && { echo "Please add github cookie secret for grafana access"; exit 1; }
+oauth2-secret-help() {
+  echo "$0 oauth2-secret NAME CLIENT_ID CLIENT_SECRET COOKIE_SECRET"
+}
 
-oc create namespace appstudio-workload-monitoring
+oauth2-secret() {
+  if [[ $# -ne 4 ]]; then
+    oauth2-secret-help
+    exit 1
+  fi
 
-oc create secret generic prometheus-proxy-config \
-  -n appstudio-workload-monitoring  \
-  --from-literal=client-id=$PROMETHEUS_GITHUB_CLIENT_ID \
-  --from-literal=client-secret=$PROMETHEUS_GITHUB_CLIENT_SECRET \
-  --from-literal=cookie-secret=$PROMETHEUS_GITHUB_COOKIE_SECRET
+  NAME=$1
+  CLIENT_ID=$2
+  CLIENT_SECRET=$3
+  COOKIE_SECRET=$4
 
-oc create secret generic grafana-oauth2-proxy-config \
-  -n appstudio-workload-monitoring  \
-  --from-literal=client-id=$GRAFANA_GITHUB_CLIENT_ID \
-  --from-literal=client-secret=$GRAFANA_GITHUB_CLIENT_SECRET \
-  --from-literal=cookie-secret=$GRAFANA_GITHUB_COOKIE_SECRET
+  oc create secret generic $NAME \
+    -n appstudio-workload-monitoring  \
+    --from-literal=client-id=$CLIENT_ID \
+    --from-literal=client-secret=$CLIENT_SECRET \
+    --from-literal=cookie-secret=$COOKIE_SECRET
+}
+
+# ----------------------------------------------------------------
+# Grafana Datasource Secrets
+# ----------------------------------------------------------------
+grafana-datasource-secret-help() {
+  echo "$0 grafana-datasource-secret NAME URL TOKEN"
+}
+
+grafana-datasource-secret() {
+
+  if [[ $# -ne 3 ]]; then
+    grafana-datasource-secret-help
+    exit 1
+  fi
+
+  NAME=$1
+  URL=$2
+  TOKEN=$3
+
+  DATA="apiVersion: 1
+datasources: 
+- name: $NAME
+  type: prometheus
+  access: proxy
+  url: $URL
+  basicAuth: false
+  withCredentials: true
+  isDefault: false
+  jsonData:
+    httpHeaderName1: 'Authorization'
+    timeInterval: 5s
+    tlsSkipVerify: true
+  secureJsonData:
+    httpHeaderValue1: 'Bearer $TOKEN'"
+  
+  oc create secret generic $NAME --from-literal=$NAME.yaml="$DATA"
+}
+
+# -----------------------------------------------------------------
+if declare -f "$1" > /dev/null
+then
+    # call arguments verbatim
+    "$@"
+else
+    # Show a helpful error
+    echo "'$1' is not a valid command" >&2
+    echo "available commands:"
+    echo "namespace                   Create the 'appstudio-workload-monitoring' namespace"
+    echo "oauth2-secret               Create the secret for Grafana's or Prometheus's OAuth2 proxy"
+    echo "grafana-datasource-secret   Create the secret for a Grafana datasource"
+    echo ""
+    exit 1
+fi
